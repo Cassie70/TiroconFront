d36f8bc47a9cdc5ec51c457e2b61a8453a16922c
Vallejo8 <anapaulinavallejoacosta@gmail.com>
2025-06-29 23:55:07 -0600

    aa

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,7 @@
 import { Routes } from '@angular/router';
 import { Index } from './pages/index/index';
 import { Registro } from './pages/registro/registro';
-import { Entrenamiento } from './pages/entrenamiento/entrenamiento';
-import {Competencia} from './pages/competencia/competencia';
+
 
 
 export const routes: Routes = [
