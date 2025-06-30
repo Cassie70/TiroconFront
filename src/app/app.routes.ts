351eb2c5024d8deba8b8cab85b4eb8227f035987
LeMiauster <maumoralesfranco@gmail.com>
2025-06-29 23:42:45 -0600

    :CCC

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { Index } from './pages/index/index';
 import { Registro } from './pages/registro/registro';
+import {Competencia} from './pages/competencia/competencia';
 
 export const routes: Routes = [
   {path: '', redirectTo: 'index', pathMatch: 'full'},
   {path: 'index', component: Index},
-  {path: 'registro', component: Registro}
+  {path: 'registro', component: Registro},
+  {path: 'competencia', component: Competencia}
 ];
---
+++ src/app/pages/competencia/competencia.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import {RouterLink} from '@angular/router';
+
+@Component({
+  selector: 'app-competencia',
+  imports: [RouterLink],
+  templateUrl: './competencia.html',
+  styleUrl: './competencia.css'
+})
+export class Competencia {
+
+}
