9ba9558f751b423416eef4eb4bedfd3455954b16
Vallejo8 <anapaulinavallejoacosta@gmail.com>
2025-06-30 00:27:22 -0600

    entrenamiento y correccion de botones

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,14 @@
 import { Routes } from '@angular/router';
 import { Index } from './pages/index/index';
 import { Registro } from './pages/registro/registro';
-import { Entrenamiento } from './pages/entrenamiento/entrenamiento';
 import {Competencia} from './pages/competencia/competencia';
+import { Entrenamiento } from './pages/entrenamiento/entrenamiento';
 
 
 export const routes: Routes = [
   {path: '', redirectTo: 'index', pathMatch: 'full'},
   {path: 'index', component: Index},
-  {path: 'registro', component: Registro}
+  {path: 'registro', component: Registro},
+  {path: 'competencia', component: Competencia},
+  {path: 'entrenamiento', component: Entrenamiento}
 ];
